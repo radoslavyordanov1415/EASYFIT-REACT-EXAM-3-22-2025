1726261a9ab5acca2584b4f11c9601d829d58363
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-03-23 15:41:14 +0100

    Add Home page with background, styling, and navigation logic

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../components/context/authContext";
+import background from "../assets/img/background.webp";
+import "../styles/Home.css";
+
+
+export default function Home() {
+    const navigate = useNavigate();
+    const { isLoggedIn } = useAuth();
+
+    const handleClick = () => {
+        if (isLoggedIn) {
+            navigate("/create");
+        } else {
+            navigate("/register");
+        }
+
+    };
+
+    return (
+        <div className="home" style={{ backgroundImage: `url(${background})` }}>
+            <div className="headerContainer">
+                <h1>Are you bored of choosing your clothes?</h1>
+                <p>One click away from the perfect fit!</p>
+                <button onClick={handleClick}>
+                    Start Creating
+                </button>
+            </div>
+        </div>
+    )
+}
