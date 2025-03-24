36e3a4e0852362616326f533ae988147990a59f2
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-03-24 13:13:18 +0100

    Add default mannequin images, positions, and dimensions

---
+++ frontend/src/components/Mannequin/constants/MannequinConstants.jsx
@@ -0,0 +1,22 @@
+export const EDITOR_WIDTH = 400
+export const EDITOR_HEIGHT = 600
+
+export const DEFAULT_WOMAN_IMAGE = "http://localhost:5005/images/female.png"
+export const DEFAULT_MAN_IMAGE = "http://localhost:5005/images/male.png"
+
+export const DEFAULT_POSITIONS = {
+    head: { x: 150, y: 20 },
+    chest: { x: 125, y: 120 },
+    legs: { x: 125, y: 270 },
+    feet: { x: 150, y: 470 },
+    default: { x: 100, y: 100 },
+}
+
+export const DEFAULT_DIMENSIONS = {
+    head: { width: 100 },
+    chest: { width: 150 },
+    legs: { height: 200 },
+    feet: { width: 100 },
+    default: { width: 100, height: 100 },
+}
+
