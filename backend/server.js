563d3745481b431167bdc6b8ccaec50532e0ec70
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-03-24 13:23:35 +0100

    Updated server configuration, middleware, and routes setup

--- backend/server.js
+++ backend/server.js
@@ -4,25 +4,27 @@ import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import dotenv from 'dotenv';
 import session from 'express-session';
-
+import path from 'path';
+import { fileURLToPath } from 'url';
 
 import authRoutes from './routes/authRoutes.js';
+import outfitRoutes from './routes/outfitRoutes.js';
+import uploadRoutes from './uploads/uploadRoutes.js';
 
-
-
-
-
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 dotenv.config();
 
 const app = express();
 
-//* Middleware Configuration
+// Middleware
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
     origin: 'http://localhost:5173',
     methods: 'GET,POST,PUT,DELETE',
     credentials: true,
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // Session handling
@@ -37,11 +39,12 @@ app.use(session({
     }
 }));
 
-//* Routes
+// Routes
 app.use('/auth', authRoutes);
-//app.use('/api/outfits');
-//app.use('/uploads');
-//app.use('/images');
+app.use('/api/outfits', outfitRoutes);
+app.use('/api', uploadRoutes);
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use('/images', express.static(path.join(__dirname, 'public/images')));
 app.use(express.static('public'));
 
 
@@ -57,7 +60,7 @@ const connectDB = async () => {
     }
 };
 
-
+// Start server
 const PORT = process.env.PORT || 5005;
 app.listen(PORT, async () => {
     await connectDB();
