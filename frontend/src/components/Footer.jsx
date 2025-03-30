e065684931e66bb754162e232f7e47acc9eed2a3
radoslavyordanov1415 <radoslavyordanov1415@gmail.com>
2025-03-30 18:33:11 +0200

    Fix social media links in Footer component + CSS

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,13 +4,25 @@ import XIcon from '@mui/icons-material/X';
 import FacebookIcon from '@mui/icons-material/Facebook';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import "../styles/Footer.css";
+
 export default function Footer() {
     return (
         <div className="footer">
             <div className="socialMedia">
-                <InstagramIcon /> <FacebookIcon /> <XIcon /> <LinkedInIcon />
+                <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+                    <InstagramIcon />
+                </a>
+                <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+                    <FacebookIcon />
+                </a>
+                <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+                    <XIcon />
+                </a>
+                <a href="https://www.linkedin.com/" target="_blank" rel="noopener noreferrer">
+                    <LinkedInIcon />
+                </a>
             </div>
             <p>&copy; 2025 EasyFit.com</p>
         </div>
-    )
+    );
 }
